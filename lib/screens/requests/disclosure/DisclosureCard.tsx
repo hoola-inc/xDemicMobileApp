822b60ee353ea96355ae38176d280082c78ecc58
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-09-24 11:03:08 +0500

    adding the avatar on disclosure request

--- lib/screens/requests/disclosure/DisclosureCard.tsx
+++ lib/screens/requests/disclosure/DisclosureCard.tsx
@@ -15,8 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with xDemic Mobile App.  If not, see <http://www.gnu.org/licenses/>.
 //
-import React from 'react'
-import { DisclosureRequestModelType } from './DisclosureRequestModel'
+import React from "react";
+import { DisclosureRequestModelType } from "./DisclosureRequestModel";
 import {
   Container,
   Text,
@@ -29,16 +29,19 @@ import {
   IndicatorBar,
   Credential,
   Theme,
-} from '@kancha'
+  Images
+} from "@kancha";
 
-import withRequestModel from './DisclosureRequestContainer'
-import SCREENS from 'xdemic/lib/screens/Screens'
+import withRequestModel from "./DisclosureRequestContainer";
+import SCREENS from "xdemic/lib/screens/Screens";
 
 /**
  * Dumb request component. Please DO NOT litter JSX template with conditional logic :D
  * All business logic should live in the RequestModel. This allows react to reuse more of the UI between renders
  */
-export const DisclosureCard: React.FC<DisclosureRequestModelType> = requestModel => {
+export const DisclosureCard: React.FC<
+  DisclosureRequestModelType
+> = requestModel => {
   return (
     <Screen
       config={Screen.Config.Scroll}
@@ -48,46 +51,66 @@ export const DisclosureCard: React.FC<DisclosureRequestModelType> = requestModel
         requestModel && (
           <Container backgroundColor={Theme.colors.primary.background}>
             <Container paddingHorizontal>
-              <Text textAlign={'center'} type={Text.Types.SectionHeader} warn={!!requestModel.error}>
-                {requestModel.error ? requestModel.error : requestModel.statsMessage}
+              <Text
+                textAlign={"center"}
+                type={Text.Types.SectionHeader}
+                warn={!!requestModel.error}
+              >
+                {requestModel.error
+                  ? requestModel.error
+                  : requestModel.statsMessage}
               </Text>
             </Container>
-            <Container flexDirection={'row'} padding>
+            <Container flexDirection={"row"} padding>
               <Container flex={1} paddingRight>
                 <Button
                   depth={1}
                   buttonText={requestModel.cancelButton.text}
                   block={Button.Block.Clear}
                   type={Button.Types.Warning}
-                  onPress={() => requestModel.cancelButton.action(requestModel.requestId)}
+                  onPress={() =>
+                    requestModel.cancelButton.action(requestModel.requestId)
+                  }
                 />
               </Container>
               <Container flex={2}>
                 <Button
-                  disabled={!!requestModel.error || requestModel.actionButton.disabled}
+                  disabled={
+                    !!requestModel.error || requestModel.actionButton.disabled
+                  }
                   buttonText={requestModel.actionButton.text}
                   block={Button.Block.Filled}
                   type={Button.Types.Primary}
                   onPress={() =>
-                    requestModel.actionButton.action(requestModel.requestId, requestModel.actionButton.actionType)
+                    requestModel.actionButton.action(
+                      requestModel.requestId,
+                      requestModel.actionButton.actionType
+                    )
                   }
                 />
               </Container>
             </Container>
           </Container>
         )
-      }>
+      }
+    >
       {requestModel && (
         <Container>
           <Banner
-            httpsResolveStatus={'OKAY'}
+            httpsResolveStatus={"OKAY"}
             backgroundImage={requestModel.appBranding.bannerImage}
-            avatar={requestModel.appBranding.profileImage}
+            avatar={
+              Images.profile.avatar
+              //requestModel.appBranding.profileImage
+            }
             requestor={requestModel.appBranding.requestor}
           />
           <IndicatorBar text={requestModel.title} />
           {requestModel.description !== null && (
-            <Container padding backgroundColor={Theme.colors.primary.background}>
+            <Container
+              padding
+              backgroundColor={Theme.colors.primary.background}
+            >
               <Text type={Text.Types.Body}>{requestModel.description}</Text>
             </Container>
           )}
@@ -95,44 +118,57 @@ export const DisclosureCard: React.FC<DisclosureRequestModelType> = requestModel
             <Section noTopBorder noTopMargin>
               {requestModel.requestItems.map((item: any, index: number) => {
                 return (
-                  <ListItem key={item.key} title={item.type} last={requestModel.requestItems.length - 1 === index}>
+                  <ListItem
+                    key={item.key}
+                    title={item.type}
+                    last={requestModel.requestItems.length - 1 === index}
+                  >
                     {item.value}
                   </ListItem>
-                )
+                );
               })}
             </Section>
           )}
           {requestModel.verifiedCredentials.length > 0 && (
             <Section noTopBorder noTopMargin>
-              {requestModel.verifiedCredentials.map((vc: any, index: number) => {
-                return (
-                  <Credential
-                    key={vc.iss + '-' + index}
-                    issuer={vc.issuer}
-                    verification={vc}
-                    claimType={vc.claimType}
-                    screen={SCREENS.Credential}
-                    componentId={requestModel.componentId}
-                  />
-                )
-              })}
+              {requestModel.verifiedCredentials.map(
+                (vc: any, index: number) => {
+                  return (
+                    <Credential
+                      key={vc.iss + "-" + index}
+                      issuer={vc.issuer}
+                      verification={vc}
+                      claimType={vc.claimType}
+                      screen={SCREENS.Credential}
+                      componentId={requestModel.componentId}
+                    />
+                  );
+                }
+              )}
             </Section>
           )}
           {requestModel.missingCredentials.length > 0 && (
             <Section noTopBorder noTopMargin>
-              {requestModel.missingCredentials.map((item: any, index: number) => {
-                return (
-                  item.essential && (
-                    <Credential key={item.claimType + '-' + index} claimType={item.claimType} spec={item} missing />
-                  )
-                )
-              })}
+              {requestModel.missingCredentials.map(
+                (item: any, index: number) => {
+                  return (
+                    item.essential && (
+                      <Credential
+                        key={item.claimType + "-" + index}
+                        claimType={item.claimType}
+                        spec={item}
+                        missing
+                      />
+                    )
+                  );
+                }
+              )}
             </Section>
           )}
         </Container>
       )}
     </Screen>
-  )
-}
+  );
+};
 
-export default withRequestModel(DisclosureCard)
+export default withRequestModel(DisclosureCard);
