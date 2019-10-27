35418876edcbef103c07e9756fde98aca93f0772
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-10-27 22:38:16 +0500

    add the alert in saga update the api url

--- lib/screens/Dashboard.tsx
+++ lib/screens/Dashboard.tsx
@@ -59,10 +59,10 @@ export class Dashboard extends React.Component<DashboardProps, DashboardState> {
     // Navigation.events().bindComponent(this);
     this.fetchCourses = this.fetchCourses.bind(this);
 
-    this.fetchSchools = this.fetchSchools.bind(this);
+    // this.fetchSchools = this.fetchSchools.bind(this);
   }
   componentDidMount() {
-    this.fetchSchools();
+    // this.fetchSchools();
     this.fetchCourses();
     this.props.getSchools(this.props.did);
   }
@@ -130,38 +130,38 @@ export class Dashboard extends React.Component<DashboardProps, DashboardState> {
   //   // updatecoursesList(json)
   // };
 
-  fetchSchools = async () => {
-    const response = await fetch(
-      `https://xdemic-api.herokuapp.com/schoolwithstudentenroll/${
-        this.props.did
-      }`
-    );
-    const json = await response.json();
-    if (!json.status) {
-      Alert.alert(
-        "Schools",
-        "Schools not found!",
-        [
-          {
-            text: "Cancel",
-            onPress: () => console.log("Cancel Pressed"),
-            style: "cancel"
-          }
-          // {
-          //   text: " Event ClearQueue",
-          //   style: "destructive",
-          //   onPress: () => console.log("on Pressed")
-          // }
-        ],
-        { cancelable: true }
-      );
-    } else {
-      this.props.addingSchool(json.data);
-      this.setState({
-        schools: json.data
-      });
-    }
-  };
+  // fetchSchools = async () => {
+  //   const response = await fetch(
+  //     `https://xdemic-api.herokuapp.com/schoolwithstudentenroll/${
+  //       this.props.did
+  //     }`
+  //   );
+  //   const json = await response.json();
+  //   if (!json.status) {
+  //     Alert.alert(
+  //       "Schools",
+  //       "Schools not found!",
+  //       [
+  //         {
+  //           text: "Cancel",
+  //           onPress: () => console.log("Cancel Pressed"),
+  //           style: "cancel"
+  //         }
+  //         // {
+  //         //   text: " Event ClearQueue",
+  //         //   style: "destructive",
+  //         //   onPress: () => console.log("on Pressed")
+  //         // }
+  //       ],
+  //       { cancelable: true }
+  //     );
+  //   } else {
+  //     this.props.addingSchool(json.data);
+  //     this.setState({
+  //       schools: json.data
+  //     });
+  //   }
+  // };
 
   render() {
     const { name, avatar, phone, did, schoolsList } = this.props;
